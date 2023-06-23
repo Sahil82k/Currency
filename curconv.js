054661013293abdff0d0cd78664b0214f50a786c
Sahil <sahil82k@gmail.com>
2023-06-23 14:48:02 +0530

    api commit

---
+++ curconv.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const conve = document.querySelector("#conv");
+  conve.addEventListener("click", async (e) => {
+    e.preventDefault();
+    //getting values
+    const crone = document.querySelector("#cco").value;
+    const crtwo = document.querySelector("#cct").value;
+    const urlconv =
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/" +
+      crone +
+      "/" +
+      crtwo +
+      ".json";
+    try {
+      const convdata = await fetch(urlconv);
+      const datacr = await convdata.json();
+      const crval=Object.values(datacr);
+      const fi=document.querySelector('#final')
+      fi.textContent(crval[1]);
+      console.log(datacr);
+    } catch {
+      console.log("an error ocurred");
+    }
+  });
+});
---
+++ main.js
@@ -0,0 +1,5 @@
+import "./index.css"
+import "./curconv.js"
+
+document.querySelector('#app').innerHTML = `
+`;
